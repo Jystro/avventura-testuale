8ea2838a3e56669a27f4cf9e3d5e37ac9bdce274
Jystro <35638179+Jystro@users.noreply.github.com>
2022-08-13 14:23:20 +0200

    First commit, InputParser library and resolution input

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "InputParser.hpp"
+#include <iostream>
+
+int main(int argc, char** argv) {
+	InputParser input(argc, argv);
+	if(!input.cmdOptionExists("-w") || !input.cmdOptionExists("-h")) {
+		std::cerr << "Please provide width and height in pixels" << std::endl;
+		return 1;
+	};
+	const int width = std::stoi(input.getCmdOption("-w"));
+	const int height = std::stoi(input.getCmdOption("-h"));
+
+	std::cout << width << 'x' << height << std::endl;
+	return 0;
+};
