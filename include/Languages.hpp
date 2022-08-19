7e879c3c3aec116c495fdc309703048a70bd2bfd
Jystro <35638179+Jystro@users.noreply.github.com>
2022-08-19 20:48:23 +0200

    Added Language header file

---
+++ include/Languages.hpp
@@ -0,0 +1,17 @@
+#ifndef LANGUAGES_HPP
+#define LANGUAGES_HPP
+
+#include <string>
+
+namespace Languages {
+	namespace Eng {
+		 std::string Menu = "Menu";
+		 std::string Start = "Start";
+		 std::string Settings = "Settings";
+	}; // ENG
+	namespace Ita {
+		 std::string Menu = "Men\151";
+	}
+}; // LANGUAGES
+
+#endif // LANGUAGES_HPP
