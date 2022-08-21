eefa3eb47eb07911a83185e9677b4c3ac8b20150
Jystro <35638179+Jystro@users.noreply.github.com>
2022-08-21 10:58:19 +0200

    Added box connectors, renamed struct item to entry and added comments
     - Added internal borders to box (connectors)
     - Changed struct Functions::item to Functions::entry
     - Better comments for box function

--- include/Functions.hpp
+++ include/Functions.hpp
@@ -4,12 +4,13 @@
 #include <string>
 
 namespace Functions {
+
 	struct terminalSize {
 		unsigned int x;
 		unsigned int y;
 	};
 
-	struct item {
+	struct entry {
 		std::string text;
 		void(*next_ptr)();
 	};
@@ -21,9 +22,17 @@ namespace Functions {
 	const char border_bottom_left[4] = "╚";
 	const char border_bottom_right[4] = "╝";
 
+	const char border_horizontal_connector_down[4] = "╦";
+	const char border_horizontal_connector_up[4] = "╩";
+
+	const char border_vertical_connector_right[4] = "╠";
+	const char border_vertical_connector_left[4] = "╣";
+
+	const char border_connector[4] = "╬";
+
 	Functions::terminalSize getTerminalSize();
 	template<const unsigned int rows, const unsigned int columns>
-	std::string fullScreenBox(const struct Functions::item (&entries)[rows][columns]);
+	std::string fullScreenBox(const struct Functions::entry (&entries)[rows][columns]);
 
 	void startMenu();
 }; // FUNCTIONS
--- src/Functions.cpp
+++ src/Functions.cpp
@@ -14,7 +14,7 @@ Functions::terminalSize Functions::getTerminalSize() {
 	terminal.y = csbi.srWindow.Right - csbi.srWindow.Left + 1;
 	terminal.x = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 	return terminal;
-}
+};
 #else
 #include <sys/ioctl.h>
 #include <stdio.h>
@@ -32,20 +32,38 @@ Functions::terminalSize Functions::getTerminalSize() {
 
 
 template<const unsigned int rows, const unsigned int columns>
-std::string Functions::fullScreenBox(const struct Functions::item (&entries)[rows][columns]) {
+std::string Functions::fullScreenBox(const struct Functions::entry (&entries)[rows][columns]) {
+	// String to be returned
 	std::string box;
+
+	// String representing different row types
 	std::string topBorder = Functions::border_top_left;
 	std::string bottomBorder = Functions::border_bottom_left;
 	std::string emptyRow = Functions::border_vertical;
+	std::string borderRow = Functions::border_vertical_connector_right;
 
 	Functions::terminalSize terminal = Functions::getTerminalSize();
 
-	// first row of border
+	// Where to put horizontal connectors
+	unsigned int columnStep = terminal.x / columns;
+
+	// Create all types of rows
 	for(int i = 0; i < terminal.x - 2; i++) {
-		topBorder += Functions::border_horizontal;
-		bottomBorder += Functions::border_horizontal;
-		emptyRow += ' ';
+		if(((i + 1) % (unsigned int)columnStep) || i + columnStep > terminal.x - 2) {
+			topBorder += Functions::border_horizontal;
+			bottomBorder += Functions::border_horizontal;
+			emptyRow += ' ';
+			borderRow += Functions::border_horizontal;
+		}
+		else {
+			topBorder += Functions::border_horizontal_connector_down;
+			bottomBorder += Functions::border_horizontal_connector_up;
+			emptyRow += Functions::border_vertical;
+			borderRow += Functions::border_connector;
+		};
 	};
+
+	// Finish each row and add \n
 	topBorder += Functions::border_top_right;
 	topBorder += '\n';
 
@@ -55,12 +73,35 @@ std::string Functions::fullScreenBox(const struct Functions::item (&entries)[row
 	emptyRow += Functions::border_vertical;
 	emptyRow += '\n';
 
+	borderRow += Functions::border_vertical_connector_left;
+	borderRow += '\n';
+
+
+	// Where to put vertical connectors
+	unsigned int rowStep = (terminal.y - 1) / rows;
+
+	// Start concatenating to get final string
 	box = topBorder;
+
 	// -2 for top and bottom, -1 for console to write command ¯\_(ツ)_/¯
 	for(int i = 0; i < terminal.y - 3; i++) {
-		box += emptyRow;
+		if(((i + 1) % (unsigned int)rowStep) || i + rowStep > terminal.y - 2) {
+			box += emptyRow;
+		}
+		else {
+			box += borderRow;
+		};
 	};
 	box += bottomBorder;
+
+	// Debugging purpose, remove later
+	for(int i = 0; i < rows; i++) {
+		for(int j = 0; j < columns; j++) {
+			std::cout << entries[i][j].text << ' ';
+		};
+		std::cout << std::endl;
+	};
+
 
 	return box;
 };
@@ -68,13 +109,13 @@ std::string Functions::fullScreenBox(const struct Functions::item (&entries)[row
 
 
 void Functions::startMenu() {
-	// options to display
-	const struct Functions::item options[2][1] = {
-		{ Languages::languages[GameState::language][Languages::STRING_START], NULL },
-		{ Languages::languages[GameState::language][Languages::STRING_QUIT], NULL }
+	// Entries to display
+	const struct Functions::entry entries[2][2] = {
+		{{ Languages::languages[GameState::language][Languages::STRING_START], NULL }, {Languages::languages[GameState::language][Languages::STRING_MENU], NULL }},
+		{{ Languages::languages[GameState::language][Languages::STRING_QUIT], NULL }, {Languages::languages[GameState::language][Languages::STRING_SETTINGS], NULL }}
 	};
 
 	// std::cout << __func__ << std::endl; Might be useful
-	std::cout << Functions::fullScreenBox<2, 1>(options);
+	std::cout << Functions::fullScreenBox<2, 2>(entries);
 	return;
 };
