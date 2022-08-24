64194685674f71f240c22aaaac462dbcb55a753c
Jystro <35638179+Jystro@users.noreply.github.com>
2022-08-24 18:02:57 +0200

    Revert "Added Functions::textBox, Functions::fullScreenTextBox and renamed drawBoxAndSetNextFunctionOnUserInput and drawTextBoxAndSetNextFunctionOnUserInput local to src/Functions.cpp, fixed typo in height"
    This reverts commit 39c636e784a25fb37ea86198291208cfa0cb5837.

--- include/Functions.hpp
+++ include/Functions.hpp
@@ -6,14 +6,9 @@
 namespace Functions {
 
 	template<const unsigned int rows, const unsigned int columns>
-	std::string box(const std::string title, const std::string (&entries)[rows][columns], const unsigned int width, unsigned int height);
-
-	std::string textBox(const std::string title, std::string text, const unsigned int width, unsigned int height);
-
+	std::string box(const std::string title, const std::string (&entries)[rows][columns], const unsigned int width, const unsigned int height);
 	template<const unsigned int rows, const unsigned int columns>
 	std::string fullScreenBox(const std::string title, const std::string (&entries)[rows][columns]);
-
-	std::string fullScreenTextBox(const std::string title, const std::string text);
 
 	// Game functions
 	void settings();
